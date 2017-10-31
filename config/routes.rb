0bc89ea01e42e5de55a0ca6339bc485c9b20d919
Marie-Charlotte Eyb <marie-charlotte@gmx.de>
2017-10-31 12:41:16 +0000

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+  # Creation
+  # get '/restaurants/new', to: "restaurants#new"
+  # post '/restaurants', to: "restaurants#create"
+
+ 	# # Read
+  # get '/restaurants', to: "restaurants#index"
+  # get '/restaurants/:id', to: "restaurants#show"
+
+  # # Update
+  # get '/restaurants/:id/edit', to: "restaurants#edit"
+  # patch '/restaurants', to: "restaurants#update"
+
+  # # Delete
+ 	# delete '/restaurants/:id', to: "restaurants#destroy"
+
+ 	resources :tasks
+ end
---
+++ db/migrate/20171031113457_id.rb
@@ -0,0 +1,4 @@
+class Id < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
